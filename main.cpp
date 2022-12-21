a75704cbfd87be93460ce5714dcb0c51ee867dc7
MaximDeGiovanni <114994423+MaximDeGiovanni@users.noreply.github.com>
2022-12-21 17:04:10 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,44 +1,53 @@
 #include <iostream>
 using namespace std;
 
-double funzione (double y) {
+double f (double y) {
     y=(pow(y,2)* cos(y))+1;
 
     return y;
+}
+double riduzione (double x) {
+    double y, z;
+    y=x*10000;
+    z=double (y)-int(y);
+
+    return z/10000;
+
+
+}
+
 int main() {
-   double a, b, x, err;
+
+    double a, b, x, err;
 
     do {
         cout << "insierire estremi" << endl;
         cin >> a >> b;
 
-    } while((funzione(a)*funzione(b))>=0);
+    } while((f(a)*f(b))>=0);
 
     do {
 
         x = ((a + b) / 2);
 
 
-        if (funzione(x) == 0) {
-            cout << x/1000000 << endl;
-            cout << funzione(x)/1000000 << endl;
-
+        if (f(x) == 0) {
+            cout << x << endl;
             break;
         }
 
 
-            if (funzione(a) * funzione(b) < 0) {
-                b = x;
-            } else {
+            if (f(x) * f(b) < 0) {
                 a = x;
+            } else {
+                b = x;
             }
 
             err = abs(((b - a) / 2));
 
-    } while (err>= 1/ pow(M_E, -6));
+    } while (err >=  pow(10, -6));
 
-    cout << x/1000000 << endl;
-    cout << funzione(x)/1000000 << endl;
+    cout << x-riduzione(x) << endl;
 
-   return 0;
+    return 0;
 }
